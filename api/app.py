bc3cb3f92e41adeb3bf2aea133f2ee390338efef
Guillaume Lefay <guillaume.lefay@gmail.com>
2021-03-19 17:53:58 +0100

    save

--- api/app.py
+++ api/app.py
@@ -1,13 +1,50 @@
 
 from fastapi import FastAPI
+import joblib as joblib
 
 app = FastAPI()
 
+model = joblib.load('../model.pickle')
 
 # define a root `/` endpoint
 @app.get("/")
 def index():
     return {"ok": True}
 
+@app.get("/predict")
+def predict(acousticness,
+            danceability,
+            duration_ms,
+            energy,
+            explicit,
+            id,
+            instrumentalness,
+            key,
+            liveness,
+            loudness,
+            mode,
+            name,
+            release_date,
+            speechiness,
+            tempo,
+            valence,
+            artis):
+  return model.predict(acousticness,
+            danceability,
+            duration_ms,
+            energy,
+            explicit,
+            id,
+            instrumentalness,
+            key,
+            liveness,
+            loudness,
+            mode,
+            name,
+            release_date,
+            speechiness,
+            tempo,
+            valence,
+            artis)
 
 # Implement a /predict endpoint
